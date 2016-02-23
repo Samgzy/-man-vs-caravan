88f9fc258e5428900380654e4ec3b0ec8c57ae00
samy gonzalez <samygz33@gmail.com>
2016-02-23 17:35:45 +0100

    caravans controller done except show and index to rent

--- app/controllers/caravans_controller.rb
+++ app/controllers/caravans_controller.rb
@@ -1,35 +1,42 @@
 class CaravansController < ApplicationController
-
-    before_action :set_caravan, only: [:show, :edit, :update, :destroy]
+  before_action :set_caravan, only: [:show, :edit, :update]
+  before_action :find_user, only: [:create, :new, :show_own, :index_own, :edit, :update, :destroy]
+  before_action :user_caravan, only: [:show_own, :edit, :destroy]
 
   def index
     @caravans = Caravan.all
   end
 
+  def index_own
+    @caravans = @user.caravans
+  end
+
   def show
+  end
+
+  def show_own
   end
 
   def new
     @caravan = Caravan.new
   end
 
+  def edit
+  end
+
   def create
-    @caravan = Caravan.new(caravan_params)
+    @caravan = @user.caravans.build(caravan_params)
     @caravan.save
     if @caravan.save
-      redirect_to caravan_path(@caravan)
+      redirect_to user_own_caravan_path(@user, @caravan)
     else
       render :new
     end
   end
 
-  def edit
-  end
-
   def update
-    @caravan.update(caravan_params)
     if @caravan.update(caravan_params)
-      redirect_to @caravan
+      redirect_to user_caravan_path(@user, @caravan)
     else
       render :edit
     end
@@ -37,9 +44,18 @@ class CaravansController < ApplicationController
 
   def destroy
     @caravan.destroy
+    redirect_to user_own_caravans_path(@user)
   end
 
   private
+
+  def user_caravan
+    @caravan = @user.caravans.find(params[:id])
+  end
+
+  def find_user
+    @user = current_user
+  end
 
   def set_caravan
     @caravan = Caravan.find(params[:id])
