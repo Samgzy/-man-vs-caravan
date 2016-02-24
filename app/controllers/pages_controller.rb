c53cded9edca368f4cd6a8d9ef434425c3a00fde
Thomas Naude <thomas.naude@gmail.com>
2016-02-24 12:11:27 +0100

    integrated cards except pictures

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :about_us]
   def home
-    @sample_caravans = Caravan.count
+    @sample_caravan1 = sample_caravan
+    @sample_caravan2 = sample_caravan
+    until @sample_caravan1 != @sample_caravan2
+      @sample_caravan2 = sample_caravan
+    end
+    @sample_caravan3 = sample_caravan
+    until (@sample_caravan1 != @sample_caravan2) && (@sample_caravan1 != @sample_caravan3) && (@sample_caravan2 != @sample_caravan3)
+      @sample_caravan3 = sample_caravan
+    end
+    @sample_caravans = [@sample_caravan1, @sample_caravan2, @sample_caravan3]
   end
 
   def about_us
+  end
+
+  private
+  def sample_caravan
+    Caravan.find((1..Caravan.count).to_a.sample)
   end
 end
