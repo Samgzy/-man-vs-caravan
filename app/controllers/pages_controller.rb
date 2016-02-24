30760adda87258e5c55ba65c564d5d0034979b19
Thomas Naude <thomas.naude@gmail.com>
2016-02-24 11:32:16 +0100

    caravan seed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :about_us]
   def home
+    @sample_caravans = Caravan.count
   end
 
   def about_us
